ab541a78830fe63c12a1905ba1011bb479f9c1f1
Neil McWilliam <neil.mcwilliam@clockworkventure.com>
2021-08-11 12:04:06 -0300

    Prevented autoload - added pre-crawl view.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\View;
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Select;
 
 class IndexController extends ControllerBase
 {
@@ -48,17 +51,6 @@ class IndexController extends ControllerBase
         $headers = get_headers($url);
         return substr($headers[0], 9, 3);
     }
-
-    // Array Needle Function
-    // UNUSED BUT KEPT HERE FOR REFERENCE
-    /* 
-    public function substrCountArray( $haystack, $needle ) {
-         $count = 0;
-         foreach ($needle as $substring) {
-              $count += substr_count( $haystack, $substring);
-         }
-         return $count;
-    } */
 
     // Get Internal Domain Unique Links
     public function countUniqueInternalLinks( $pageContent ) {
@@ -199,166 +191,6 @@ EOF;
         return round($time_taken, 2);
     }
 
-    // Calculate all pages average load time
-    // UNUSED, BUT KEPT HERE FOR REFERENCE
-    /* 
-    public function averagePageLoadTime(){ 
-
-        // Set urls to scan
-        $pageUrl1 = 'https://agencyanalytics.com/feature/seo-tools'; // seo tools page
-        $pageUrl2 = 'https://agencyanalytics.com/pricing'; // pricing page
-        $pageUrl3 = 'https://agencyanalytics.com/feature/automated-marketing-reports'; // automated marketing reports
-        $pageUrl4 = 'https://agencyanalytics.com/feature/white-label'; // white label
-        $pageUrl5 = 'https://agencyanalytics.com/feature/linkedin-dashboard'; // linkedin dashboard
-
-        // Use file_get_contents to gather data from each page
-        $pageContent1 = file_get_contents($pageUrl1); 
-        $pageContent2 = file_get_contents($pageUrl2); 
-        $pageContent3 = file_get_contents($pageUrl3); 
-        $pageContent4 = file_get_contents($pageUrl4); 
-        $pageContent5 = file_get_contents($pageUrl5); 
-
-        // Create an empty array to hold the results
-        $arrayLoadTimes = array(); 
-
-        // Page 1 
-
-        // Create a new DOM Document to hold page structure
-        $doc1 = new DOMDocument();
-
-        // Grab the current time
-        $start_time1 = microtime(TRUE); 
-
-        // Specify the html content
-        $myhtml1 = <<<EOF
-        $pageContent1
-EOF;
-
-        // load the html
-        $doc1->loadHTML($myhtml1);
-
-        // Calculate the end time
-        $end_time1 = microtime(TRUE);
-
-        // Calculate the time taken
-        $time_taken1 =($end_time1 - $start_time1)*1000;
-        $time_taken1 = round($time_taken1,5);
-
-        // Add the load time to the array 
-        $arrayLoadTimes[] = $time_taken1; 
-
-        // Page 2 
-
-        // Create a new DOM Document to hold page structure
-        $doc2 = new DOMDocument();
-
-        // Grab the current time
-        $start_time2 = microtime(TRUE); 
-
-        // Specify the html content
-        $myhtml2 = <<<EOF
-        $pageContent2
-EOF;
-
-        // Load the html
-        $doc2->loadHTML($myhtml2);
-
-        // Calculate the end time
-        $end_time2 = microtime(TRUE); 
-
-        // Calculate the time taken
-        $time_taken2 =($end_time2 - $start_time2)*1000;
-        $time_taken2 = round($time_taken2,5);
-
-        // Add the load time to the array 
-        $arrayLoadTimes[] = $time_taken2; 
-
-        // Page 3 
-
-        // Create a new DOM Document to hold page structure
-        $doc3 = new DOMDocument();
-
-        // Grab the current time
-        $start_time3 = microtime(TRUE); 
-
-        // Load the 3rd page html content
-        $myhtml3 = <<<EOF
-        $pageContent3
-EOF;
-
-        // Load the html content
-        $doc3->loadHTML($myhtml3);
-
-        // Calculate the end time
-        $end_time3 = microtime(TRUE); 
-
-        // Calculate the time taken
-        $time_taken3 =($end_time3 - $start_time3)*1000;
-        $time_taken3 = round($time_taken3,5);
-
-        // Add the load time to the array 
-        $arrayLoadTimes[] = $time_taken3; 
-
-        // Page 4 
-
-        // Create a new DOM Document to hold page structure
-        $doc4 = new DOMDocument();
-
-        // Grab the current time
-        $start_time4 = microtime(TRUE); 
-
-        // Load the 4th page html content
-        $myhtml4 = <<<EOF
-        $pageContent4
-EOF;
-
-        // Load the html
-        $doc4->loadHTML($myhtml4);
-
-        // Calculate the end time
-        $end_time4 = microtime(TRUE); 
-
-        // Calculate the time taken
-        $time_taken4 =($end_time4 - $start_time4)*1000;
-        $time_taken4 = round($time_taken4,5);
-
-        // Add the load time to the array 
-        $arrayLoadTimes[] = $time_taken4; 
-
-        // Page 5 
-
-        // Create a new DOM Document to hold page structure
-        $doc5 = new DOMDocument();
-
-        // Grab the current time
-        $start_time5 = microtime(TRUE); 
-
-        // Load the 5th page html content
-        $myhtml5 = <<<EOF
-        $pageContent5
-EOF;
-
-        // Load the html
-        $doc5->loadHTML($myhtml5);
-
-        // Calculate the end time
-        $end_time5 = microtime(TRUE); 
-
-        // Calculate the time taken
-        $time_taken5 =($end_time5 - $start_time5)*1000;
-        $time_taken5 = round($time_taken5,5);
-
-        // Add the load time to the array 
-        $arrayLoadTimes[] = $time_taken5; 
-
-        // Calculate the load time average, excluding empties 
-        $a = array_filter($arrayLoadTimes);
-        $average = array_sum($a)/count($a);
-
-        return round($average, 2);
-    } */
-
-
     // Get unique images on a page
     public function countUniqueImages( $pageContent ) {
 
@@ -387,7 +219,4 @@ EOF;
         $uniq = count(array_unique($array));
         return $uniq;
     }
-
-
 }
-
